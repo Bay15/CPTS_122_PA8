c23e11a973f906cc5e1598fb0da1d5b4e1e40914
Bay15 <bayley@dreamen.net>
2020-11-30 13:35:09 -0800

    Add files via upload

---
+++ SFML_Base/main.cpp
@@ -0,0 +1,149 @@
+#include <SFML/Graphics.hpp>
+
+class TileMap : public sf::Drawable, public sf::Transformable
+{
+public:
+
+    bool load(const std::string& tileset, sf::Vector2u tileSize, const int* tiles, unsigned int width, unsigned int height)
+    {
+        // load the tileset texture
+        if (!m_tileset.loadFromFile(tileset))
+            return false;
+
+        // resize the vertex array to fit the level size
+        m_vertices.setPrimitiveType(sf::Quads);
+        m_vertices.resize(width * height * 4);
+
+        // populate the vertex array, with one quad per tile
+        for (unsigned int i = 0; i < width; ++i)
+            for (unsigned int j = 0; j < height; ++j)
+            {
+                // get the current tile number
+                int tileNumber = tiles[i + j * width];
+
+                // find its position in the tileset texture
+                int tu = tileNumber % (m_tileset.getSize().x / tileSize.x);
+                int tv = tileNumber / (m_tileset.getSize().x / tileSize.x);
+
+                // get a pointer to the current tile's quad
+                sf::Vertex* quad = &m_vertices[(i + j * width) * 4];
+
+                // define its 4 corners
+                quad[0].position = sf::Vector2f(i * tileSize.x, j * tileSize.y);
+                quad[1].position = sf::Vector2f((i + 1) * tileSize.x, j * tileSize.y);
+                quad[2].position = sf::Vector2f((i + 1) * tileSize.x, (j + 1) * tileSize.y);
+                quad[3].position = sf::Vector2f(i * tileSize.x, (j + 1) * tileSize.y);
+
+                // define its 4 texture coordinates
+                quad[0].texCoords = sf::Vector2f(tu * tileSize.x, tv * tileSize.y);
+                quad[1].texCoords = sf::Vector2f((tu + 1) * tileSize.x, tv * tileSize.y);
+                quad[2].texCoords = sf::Vector2f((tu + 1) * tileSize.x, (tv + 1) * tileSize.y);
+                quad[3].texCoords = sf::Vector2f(tu * tileSize.x, (tv + 1) * tileSize.y);
+            }
+
+        return true;
+    }
+
+private:
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+    {
+        // apply the transform
+        states.transform *= getTransform();
+
+        // apply the tileset texture
+        states.texture = &m_tileset;
+
+        // draw the vertex array
+        target.draw(m_vertices, states);
+    }
+
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset;
+};
+int main()
+{
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(576, 576), "Tilemap");
+
+    // define the level with an array of tile indices
+    const int level[] =
+    {
+        1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 0, 0, 0, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3,
+        0, 1, 0, 0, 2, 0, 3, 3, 3, 0, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 0, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2, 0, 0,
+        0, 0, 1, 0, 3, 0, 2, 2, 0, 0, 1, 1, 1, 1, 2, 0,
+        2, 0, 1, 0, 3, 0, 2, 2, 2, 0, 1, 1, 1, 1, 1, 1,
+        0, 0, 1, 0, 3, 2, 2, 2, 0, 0, 0, 0, 1, 1, 1, 1,
+    };
+
+    const int level2[] =
+    {
+        0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 1, 1, 1, 1, 1, 4, 0,
+        0, 0, 1, 0, 0, 0, 3, 0, 0,
+        0, 3, 1, 0, 1, 1, 1, 1, 0,
+        0, 0, 1, 0, 1, 0, 1, 0, 0,
+        0, 1, 1, 1, 1, 0, 1, 1, 0,
+        0, 1, 3, 0, 0, 3, 0, 1, 0,
+        0, 1, 1, 1, 1, 1, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 2, 0,
+    };
+
+    // create the tilemap from the level definition
+    TileMap map;
+    if (!map.load("tilesetMain.png", sf::Vector2u(64, 64), level2, 9, 9))
+        return -1;
+
+    sf::Sprite sprite;
+    sf::Texture texture;
+    texture.loadFromFile("BlobSheet.png");
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+    sprite.setPosition(64*7 +16, 64*1 + 16); 
+
+
+    // run the main loop
+    while (window.isOpen())
+    {
+        // handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // draw the map
+        window.clear();
+        window.draw(map);
+        window.draw(sprite);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            sprite.move(0, .25);
+            sprite.setTextureRect(sf::IntRect(0, 0, 32, 32));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            sprite.move(0, -.25);
+            sprite.setTextureRect(sf::IntRect(32, 0, 32, 32));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+        {
+            sprite.move(-.25,0);
+            sprite.setTextureRect(sf::IntRect(64, 0, 32, 32));
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+        {
+            sprite.move(.25, 0);
+            sprite.setTextureRect(sf::IntRect(96, 0, 32, 32));
+        }
+
+        window.display();
+    }
+
+    return 0;
+}
