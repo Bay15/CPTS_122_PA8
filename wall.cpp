b7b3762b470ec7cdef7b132fe3b77af899905ca8
ChrisLum93 <74024206+ChrisLum93@users.noreply.github.com>
2020-12-04 18:46:47 -0800

    Update wall.cpp

--- wall.cpp
+++ wall.cpp
@@ -1,18 +1,18 @@
 #include "wall.h"
 
-wall::wall()
+wall::wall() //establishes size and color
 {
 	rect.setSize(sf::Vector2f(64, 64));
 	rect.setPosition(128, 128);
 	rect.setFillColor(sf::Color::Cyan);
 }
-wall::wall(int height, int length)
+wall::wall(int height, int length) //establishes size passed into the function
 {
 	rect.setSize(sf::Vector2f(64, 64));
 	rect.setPosition(height, length);
 	rect.setFillColor(sf::Color::Transparent);
 }
 
-wall::~wall()
+wall::~wall() //destructor
 {
 }
