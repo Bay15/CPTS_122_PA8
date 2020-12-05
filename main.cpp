a48890323ddc61aa04567d70bfabf943e371b650
Bay15 <bayley@dreamen.net>
2020-12-04 20:47:28 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -77,24 +77,24 @@ int main()
     std::vector<wall> winArray;
     std::vector<wall> lavaArray;
     TileMap map;
-    sf::View view1, view2;
-    view1.setCenter(sf::Vector2f(512, 384));
+    sf::View view1, view2; //views for the game
+    view1.setCenter(sf::Vector2f(512, 384)); //screen
     view1.setSize(sf::Vector2f(1024, 768));
-    view2.setCenter(sf::Vector2f(80, 80));
+    view2.setCenter(sf::Vector2f(80, 80)); //player
     view2.setSize(sf::Vector2f(200, 200));
     
 
-
-    if (!map.load("Resources/tileset.png", sf::Vector2u(pixel_length, pixel_length), level, width, height, wallArray, winArray, lavaArray))
+    //making sure the texture loads properly for the tileset
+    if (!map.load("Resources/tileset.png", sf::Vector2u(pixel_length, pixel_length), level, width, height, wallArray, winArray, lavaArray)) 
         return -1;
-
+    //while the window is open, the game runs
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (window.pollEvent(event)) //if the window is said to be closed
         {
             if (event.type == sf::Event::Closed)
-                window.close();
+                window.close(); //window is closed and game stops 
         }
 
         window.clear();
@@ -109,20 +109,22 @@ int main()
         {
             window.clear(); //clears any of the mainscreen//updates game screen
  
-
-            if (!pause.checkIfPaused() && player1.getAlive() && !player1.getWin()) //player can only move as long as the window is not paused
+            //player can only move as long as the game isn't paused, they're alive, and they hadn't won
+            if (!pause.checkIfPaused() && player1.getAlive() && !player1.getWin()) 
             {
-                window.setView(view2);
-                player1.move(wallArray, winArray, lavaArray, view2);
+                window.setView(view2); //sets the view to be close to player
+                player1.move(wallArray, winArray, lavaArray, view2); //player moves
             }
             window.draw(map);//draws map
             window.draw(player1.sprite);//draws sprite
-
+            
+            //checks to see if the player won or died
             if (endGame.checkIfEnd(window, player1.getAlive(), player1.getWin()))
             {
-                window.setView(view1);
+                window.setView(view1); //sets to an end screen
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) //redirects user back to Main Menu
                 {
+                    //resetting everything
                     pause.setExit(false);
                     pause.setPause(false);
                     main.setStart(false);
@@ -131,14 +133,16 @@ int main()
                     window.clear();
                 }
             }
-            if (pause.pauseScreenGame(window)) //pauses the game
-            {
-                pause.makeBackground(window);
-                window.setView(view1);
             
+            if (pause.pauseScreenGame(window)) //if the game is paused
+            {
+                pause.makeBackground(window); //make paused BG
+                window.setView(view1); //sets it to screen view
             }
+            
             else if (pause.checkIfExit()) //Exits to main menu
             {
+                //resetting everything
                 pause.setExit(false);
                 pause.setPause(false);
                 main.setStart(false);
