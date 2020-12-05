a9e0d7d93f21caf98d43f9fa1d5c7152d6f5d843
ChrisLum93 <74024206+ChrisLum93@users.noreply.github.com>
2020-12-04 19:03:50 -0800

    Update pauseScreen.cpp

--- pauseScreen.cpp
+++ pauseScreen.cpp
@@ -1,63 +1,63 @@
 #include "pauseScreen.h"
 
-void pauseScreen::setPause(bool inUse)
+void pauseScreen::setPause(bool inUse) //establishes if the puase menu is up as  true or false
 {
 	pauseGame = inUse;
 }
 
 
-bool pauseScreen::checkIfPaused()
+bool pauseScreen::checkIfPaused() //returns true if the puase menu is up
 {
 	return pauseGame;
 
 }
 
-void pauseScreen::setExit(bool inUse)
+void pauseScreen::setExit(bool inUse) //establishes if the game was exited as true or false
 {
 	exitGame = inUse;
 }
 
 
-bool pauseScreen::checkIfExit()
+bool pauseScreen::checkIfExit() //returns true if the game has been exited
 {
 	return exitGame;
 }
 
 
-bool pauseScreen::pauseScreenGame(sf::RenderWindow& window)
+bool pauseScreen::pauseScreenGame(sf::RenderWindow& window) //puases the game
 {
 	
-	if (!pauseGame)
+	if (!pauseGame) //if the game is not already paused
 	{
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) //if the escape key is pressed
 		{
-			pauseGame = true;
+			pauseGame = true; //pause the game
 			int temp = time(NULL) + 1;
 
 			while (temp > time(NULL));
 		}
 		else
 		{
-			pauseGame = false;
+			pauseGame = false; //otherwise don't
 		}
 	}
-	else if (pauseGame)
+	else if (pauseGame) //if the game is already paused
 	{
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) //if the escape key is pressed
 		{
-			pauseGame = false;
+			pauseGame = false; //unpause the game
 			int temp = time(NULL) + 1;
 
 			while (temp > time(NULL));
 		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter))
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)) //if the enter key is pressed
 		{
-			pauseGame = false;
+			pauseGame = false; //unpause the game and exit 
 			exitGame = true;
 		}
-		else
+		else //otherwise pause game remains true
 		{
-			pauseGame = true;
+			pauseGame = true; 
 		}
 
 	}
